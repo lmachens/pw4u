bf5c401377ffd3bc564c6c23c75fdafa4148d7bf
Leon Machens <leon.machens@googlemail.com>
2020-11-16 11:57:55 +0100

    🗃 Add MongoDB (#8)
    * 🗃 Add MongoDB connection
    
    * 🗃 Insert password into db
    
    * 🔒 Add dotenv
    
    * Read password from database
    
    * 🔥 Remove unused code
    
    * Update password if already exist
    
    * Prepare deletePassword

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
+require("dotenv").config();
+
 const { readCommandLineArguments } = require("./lib/commandLine");
+const { connect, close } = require("./lib/database");
 const { getPassword, setPassword } = require("./lib/passwords");
 const { askForMasterPassword } = require("./lib/questions");
 const { isMasterPasswordCorrect } = require("./lib/validation");
 
 async function run() {
-  const masterPassword = await askForMasterPassword();
+  try {
+    console.log("Connecting to database...");
+    await connect(process.env.MONGO_DB_URI, process.env.MONGO_DB_NAME);
+    console.log("Connected to database 🎉");
 
-  if (!(await isMasterPasswordCorrect(masterPassword))) {
-    console.error("You are not welcome here! 👿 Try again!");
-    return run();
-  }
+    const masterPassword = await askForMasterPassword();
 
-  const [passwordName, newPasswordValue] = readCommandLineArguments();
-  if (!passwordName) {
-    console.error("Missing password name!");
-    return process.exit(9);
-  }
+    if (!(await isMasterPasswordCorrect(masterPassword))) {
+      console.error("You are not welcome here! 👿 Try again!");
+      return run();
+    }
 
-  if (newPasswordValue) {
-    await setPassword(passwordName, newPasswordValue);
-    console.log(`Password ${passwordName} set 🎉`);
-  } else {
-    const passwordValue = await getPassword(passwordName);
-    console.log(`Your password is ${passwordValue} 🎉`);
+    const [passwordName, newPasswordValue] = readCommandLineArguments();
+    if (!passwordName) {
+      console.error("Missing password name!");
+      return process.exit(9);
+    }
+
+    if (newPasswordValue) {
+      await setPassword(passwordName, newPasswordValue);
+      console.log(`Password ${passwordName} set 🎉`);
+    } else {
+      const passwordValue = await getPassword(passwordName);
+      console.log(`Your password is ${passwordValue} 🎉`);
+    }
+  } catch (error) {
+    if (error.message.match("E11000")) {
+      console.error("DUPLICATE KEY JO!");
+    } else {
+      console.error(error);
+    }
+  } finally {
+    await close();
   }
 }
 
